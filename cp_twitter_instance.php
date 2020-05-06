c5b9215ef7038ae2a5a7c36edb093373fe8af3ca
Hakusye <ryoga.1023.35s@gmail.com>
2020-05-06 21:00:11 +0900

    add auto tweet with Oauth

--- cp_twitter_instance.php
+++ cp_twitter_instance.php
@@ -2,10 +2,10 @@
 require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$consumerKey       = "hzTH5JMYenuZsPdrqGzK9bToK";
-$consumerSecret    = "Ef8Mh6V3hQKoIgWQex9DKr5S7PBhUJddT9lZvENQ7GfMKcOmNI";
-$accessToken       = "3303913819-jrH0Ob7PSdZL2lhDUFQSgSY2WslC1k1NWswC4JL";
-$accessTokenSecret = "jkFsU19wKEMp9B3ld9AVqoLqEiW1XRwTQkySuSeVi2WrK";
+$consumerKey       = "";
+$consumerSecret    = "";
+$accessToken       = "";
+$accessTokenSecret = "";
 
 $twitter = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 $result = $twitter->post("statuses/update", array("status" => "twitterOAuthからツイート"));
